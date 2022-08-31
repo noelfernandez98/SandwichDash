5f8cfa9032ba202706b8e2ecf6529224939fe326
marcwayn8 <juniormarch48@gmail.com>
2022-08-31 19:09:04 +0000

    almost complete

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
 kaboom({
 	// Scale the whole game up
-	scale: 1,
+	scale: 2,
 	// Set the default font
 	font: "sinko",
 })
 ;
  loadSprite("clearsky", "DinerPic.jpg")
-let background = add([
-  sprite("clearsky"),
-  pos(width() / 2, height() / 2),
-  origin("center"),
-  scale(1),
-  fixed()
-]);
-
-
-loadSprite("meat", "meat.png")
+ loadSprite("meat", "meat.png")
 loadSprite("lettuce", "lettuce.png")
 loadSprite("onions", "onions.png")
 loadSprite("tomatoes", "tomatoes.png")
@@ -24,8 +15,35 @@ loadSprite("bread2", "bread.png")
 loadSprite("cheese", "cheese.png")
 // Load assets
 loadSprite("bean", "plate.png")
+
+
+scene('intro', ()=>{
+	let background = add([
+	  sprite("clearsky"),
+	  pos(width() / 2, height() / 2),
+	  origin("center"),
+	  scale(1),
+	  fixed()
+	]);
+	const start=document.getElementById("start")
+     start.addEventListener("click", function(){
+   start.hidden=true,
+ 
+	go('game')
+     })
+})
+go('intro')
+
+scene('game', ()=>{
+let background = add([
+	  sprite("clearsky"),
+	  pos(width() / 2, height() / 2),
+	  origin("center"),
+	  scale(1),
+	  fixed()
+	]);
 // Define player movement speed (pixels per second)
-const SPEED = 320
+const SPEED = 320;
 // Add player game object
 const player = add([
 	sprite("bean"),
@@ -53,7 +71,7 @@ onClick(() => {
 })
 add([
 	// text() component is similar to sprite() but renders text
-	text("Press arrow keys", { width: width() / 2 }),
+	text("Press arrow keys"),
 	pos(550, 12),
 ])
 
@@ -142,7 +160,7 @@ loop(50, () => {
 	const y = rand(0, height())
 
 	add([
-		sprite("bread2"),
+		sprite("cheese"),
 		pos(x, y),
 		// Both objects must have area() component to enable collision detection between
 		area(),
@@ -151,198 +169,75 @@ loop(50, () => {
 	])
 })
 }
+var score=0
+add([
+	score,
+	pos(10, 10),
+		// Both objects must have area() component to enable collision detection between
+
+	
+	])
 
 
 
      const howToPlay=document.getElementById("instructions")
      howToPlay.addEventListener("click",function(){    document.open("instructions.html","How To Play", "width=600,height=600")})
-     const start=document.getElementById("start")
-     start.addEventListener("click", function(){
-   start.hidden=true,
-   howToPlay.hidden=true
-  
-     })
      
-     //Collison section
      
-loadSound("OtherworldlyFoe", "music.mp3")
+//      loadSound("OtherworldlyFoe", "music.m4a")
 
-// play() to play audio
-// (This might not play until user input due to browser policy)
-const music = play("OtherworldlyFoe", {
-	loop: true,
+// // play() to play audio
+// // (This might not play until user input due to browser policy)
+// const music = play("OtherworldlyFoe", {
+// 	loop: true,
+// })
+
+// // Adjust global volume
+// volume(0.5)
+
+// const label = add([
+// 	text(),
+// ])
+
+// function updateText() {
+// 	label.text = `
+// ${music.isPaused() ? "Paused" : "Playing"}
+// Time: ${music.time().toFixed(2)}
+// Tolume: ${music.volume().toFixed(2)}
+// Tetune: ${music.detune().toFixed(2)}
+// 	`.trim()
+// }
+
+// updateText()
+
+// // Update text every frame
+// onUpdate(updateText)
+
+// // Adjust music properties through input
+// onKeyPress("space", () => {
+// 	if (music.isPaused()) {
+// 		music.play()
+// 	} else {
+// 		music.pause()
+// 	}
+// })
+
+// onKeyPress("up", () => music.volume(music.volume() + 0.1))
+// onKeyPress("down", () => music.volume(music.volume() - 0.1))
+// onKeyPress("left", () => music.detune(music.detune() - 100))
+// onKeyPress("right", () => music.detune(music.detune() + 100))
+// onKeyPress("escape", () => music.stop())
+
+// const keyboard = "awsedftgyhujk"
+
+// // Simple piano with "bell" sound and the second row of a QWERTY keyboard
+// for (let i = 0; i < keyboard.length; i++) {
+// 	onKeyPress(keyboard[i], () => {
+// 		play("bell", {
+// 			// The original "bell" sound is F, -500 will make it C for the first key
+// 			detune: i * 100 - 500,
+// 		})
+// 	})
+// }
+
 })
-
-// Adjust global volume
-volume(0.5)
-
-const label = add([
-	text(),
-])
-
-function updateText() {
-	label.text = `
-${music.isPaused() ? "Paused" : "Playing"}
-Time: ${music.time().toFixed(2)}
-
-	`.trim()
-}
-
-updateText()
-
-// Update text every frame
-onUpdate(updateText)
-
-// Adjust music properties through input
-onKeyPress("space", () => {
-	if (music.isPaused()) {
-		music.play()
-	} else {
-		music.pause()
-	}
-})
-
-onKeyPress("up", () => music.volume(music.volume() + 0.1))
-onKeyPress("down", () => music.volume(music.volume() - 0.1))
-onKeyPress("left", () => music.detune(music.detune() - 100))
-onKeyPress("right", () => music.detune(music.detune() + 100))
-onKeyPress("escape", () => music.stop())
-
-const keyboard = "awsedftgyhujk"
-
-// Simple piano with "bell" sound and the second row of a QWERTY keyboard
-for (let i = 0; i < keyboard.length; i++) {
-	onKeyPress(keyboard[i], () => {
-		play("bell", {
-			// The original "bell" sound is F, -500 will make it C for the first key
-			detune: i * 100 - 500,
-		})
-	})
-}
-var score=0
-
-   onCollide('lettuce', 'bean', (lettuce) => {
-     destroy(lettuce)
-     
-   })
-// player.onCollide("lettuce", (lettuce) => {
-// 	destroy(lettuce)
-// })
-
-
-// loop(1, () => {
-//     // add tree
-//     add([
-//       sprite('meat'),
-//       pos(rand(vec2(width())).x, 10),
-//       `tag1`,
-//       area(),
-//       scale(.2),
-//       move(DOWN, 30),
-//     ])
-//   });
-//   loop(1, () => {
-//     // add tree
-//     add([
-//       sprite('lettuce'),
-//       pos(rand(vec2(width())).x, 10),
-//       `tag1`,
-//       area(),
-//       scale(.2),
-//       move(DOWN, 40),
-//     ])
-//   });
-//   loop(1, () => {
-//     // add tree
-//     add([
-//       sprite('onions'),
-//       pos(rand(vec2(width())).x, 10),
-//       `tag1`,
-//       area(),
-//       scale(.2),
-//       move(DOWN, 50),
-//     ])
-//   });
-//   loop(1, () => {
-//     // add tree
-//     add([
-//       sprite('bread1'),
-//       pos(rand(vec2(width())).x, 10),
-//       `tag1`,
-//       area(),
-//       scale(.2),
-//       move(DOWN, 60),
-//     ])
-//   });
-//   loop(1, () => {
-//     // add tree
-//     add([
-//       sprite('bread2'),
-//       pos(rand(vec2(width())).x, 10),
-//       `tag1`,
-//       area(),
-//       scale(.2),
-//       move(DOWN, 70),
-//     ])
-//   });
-//   loop(10, () => {
-//     // add tree
-//     add([
-//       sprite('tomatoes'),
-//       pos(rand(vec2(width())).x, 10),
-//       `tag1`,
-//       area(20),
-//       scale(.2),
-//       move(DOWN, 80),
-//     ])
-//   });
-//   loop(1, () => {
-//     // add tree
-//     add([
-//       sprite('tomatoes'),
-//       pos(rand(vec2(width())).x, 10),
-//       `tag1`,
-//       area(),
-//       scale(.2),
-//       move(DOWN, 100),
-//     ])
-//   });
-
-
-// .onCollide() is provided by area() component, it registers an event that runs when an objects collides with another object with certain tag
-// In this case we destroy (remove from game) the enemy when player hits one
-// player.onCollide("enemy", (enemy) => {
-// 	destroy(enemy)
-// })
-
-// // .clicks() is provided by area() component, it registers an event that runs when the object is clicked
-// player.onClick(() => {
-// 	debug.log("what up")
-// })
-
-  
-  
-  
-  
-  //Destroy Randomly
-  
-  
- // loop(0.5, () => {
-	// // Execute something after 3 seconds.
-	// wait(3, () => {
-	// 	destroy("lettuce")
-	// })
-
-
-
- // loop(0.5, () => {
-	// // Execute something after 3 seconds.
-	// wait(3, () => {
-	// 	destroy("meat")
-	// })
-	
- // loop(0.5, () => {
-	// // Execute something after 3 seconds.
-	// wait(3, () => {
-	// 	destroy("onions")
-	// })
