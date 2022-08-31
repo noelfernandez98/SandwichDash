52f2532af2e3626f3e5da1930910ea2030a9b7c9
marcwayn8 <juniormarch48@gmail.com>
2022-08-31 14:59:10 +0000

    images working

--- index.js
+++ index.js
@@ -1,18 +1,73 @@
-kaboom()
+kaboom({
+	// Scale the whole game up
+	scale: 2,
+	// Set the default font
+	font: "sinko",
+})
 
-const pauseAction = () => {
-    const newArr = document.querySelectorAll('pause');
-   let pauseCheck
-    if(pauseCheck) {
-        newArr.forEach(e => {
-            if(e.id !== 'pause'){
-                e.disabled = true;
-            }  });
-        
-        pauseCheck = false;
-        
-        clearTimeout();
-     }
-}
+ loadSprite("clearsky", "DinerPic.jpg")
+let background = add([
+  sprite("clearsky"),
+  pos(width() / 2, height() / 2),
+  origin("center"),
+  scale(1),
+  fixed()
+]);
+loadSprite("meat", "meat.png")
+loadSprite("lettuce", "lettuce.png")
+loadSprite("onions", "onions.png")
+loadSprite("tomatoes", "tomatoes.png")
+loadSprite("bread1", "bread.png")
+loadSprite("bread2", "bread.png")
+// Load assets
+loadSprite("bean", "character.png")
+// Define player movement speed (pixels per second)
+const SPEED = 320
+// Add player game object
+const player = add([
+	sprite("bean"),
+	// center() returns the center point vec2(width() / 2, height() / 2)
+	pos(center()),
+])
+// onKeyDown() registers an event that runs every frame as long as user is holding a certain key
+onKeyDown("left", () => {
+	// .move() is provided by pos() component, move by pixels per second
+	player.move(-SPEED, 0)
+})
+onKeyDown("right", () => {
+	player.move(SPEED, 0)
+})
+onKeyDown("up", () => {
+	player.move(0, -SPEED)
+})
+onKeyDown("down", () => {
+	player.move(0, SPEED)
+})
+// onClick() registers an event that runs once when left mouse is clicked
+onClick(() => {
+	// .moveTo() is provided by pos() component, changes the position
+	player.moveTo(mousePos())
+})
+add([
+	// text() component is similar to sprite() but renders text
+	text("Press arrow keys", { width: width() / 2 }),
+	pos(12, 12),
+])
+loop(1, () => {
+    // add tree
+    add([
+      sprite('meat'),sprite('lettuce'),
+      pos(rand(vec2(width())).x, 10),
+      `tag1`,
+      area(),
+      scale(.2),
+      move(DOWN, 240),
+    ])
+  });
      const howToPlay=document.getElementById("instructions")
      howToPlay.addEventListener("click",function(){    document.open("instructions.html","How To Play", "width=600,height=600")})
+     const start=document.getElementById("start")
+     start.addEventListener("click", function(){
+   start.hidden=true
+   howToPlay.hidden=true
+     })
