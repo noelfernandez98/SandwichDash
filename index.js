e3c37f072b3b6297ca1a6cffc36f2d77fbb8fade
Noel Fernandez <noelfernandez98@gmail.com>
2022-08-31 15:14:01 -0400

    finalizing before merge

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 kaboom({
-	// Scale the whole game up
-	scale: 1,
+  height: 755,
+  width: 1500,
 	// Set the default font
 	font: "sinko",
-})
-;
- loadSprite("clearsky", "DinerPic.jpg")
+});
+
+loadSprite("clearsky", "DinerPic.jpg")
+loadSprite("meat", "meat.png")
+loadSprite("lettuce", "lettuce.png")
+loadSprite("onions", "onions.png")
+loadSprite("tomatoes", "tomatoes.png")
+loadSprite("bread1", "bread.png")
+loadSprite("bread2", "bread.png")
+loadSprite("player", "character.png");
+loadSprite("bean", "plate.png")
+
 let background = add([
   sprite("clearsky"),
   pos(width() / 2, height() / 2),
@@ -13,22 +22,17 @@ let background = add([
   scale(1),
   fixed()
 ]);
-loadSprite("meat", "meat.png")
-loadSprite("lettuce", "lettuce.png")
-loadSprite("onions", "onions.png")
-loadSprite("tomatoes", "tomatoes.png")
-loadSprite("bread1", "bread.png")
-loadSprite("bread2", "bread.png")
-// loadSprite("cheese", "cheese.png")
-// Load assets
-loadSprite("bean", "plate.png")
-// Define player movement speed (pixels per second)
-const SPEED = 320
-// Add player game object
 
-// onKeyDown() registers an event that runs every frame as long as user is holding a certain key
+let player = add([
+  sprite("player"),
+  pos(width() / 2, height()/1.4),
+  origin("center"),
+  scale(3)
+]);
+
+const SPEED = 320
+
 onKeyDown("left", () => {
-	// .move() is provided by pos() component, move by pixels per second
 	player.move(-SPEED, 0)
 })
 onKeyDown("right", () => {
@@ -36,11 +40,10 @@ onKeyDown("right", () => {
 })
 
 const options = ["meat", "lettuce", "onions", "bread2", "tomatoes"]
-const randomizer = () => options[Math.floor(Math.random() * arr.length)]
+const randomizer = () => options[Math.floor(Math.random() * options.length)]
 
 const looper = () => {
   loop(1, () => {
-    // add tree
     add([
       sprite(randomizer()),
       pos(rand(vec2(width())).x, 10),
@@ -54,21 +57,29 @@ const looper = () => {
 
 looper()
 
+ add([
+  sprite("bread2"),
+  // pos(height() / 3, width() / 3),
+   pos(player / 5, width() / 3),
+
+   scale(.1),
+  fixed()
+])
 
 add([
-	// text() component is similar to sprite() but renders text
 	text("Press arrow keys", { width: width() / 2 }),
 	pos(12, 12),
 ])
 
      const howToPlay=document.getElementById("instructions")
-     howToPlay.addEventListener("click",function(){    document.open("instructions.html","How To Play", "width=600,height=600")})
-     const start=document.getElementById("start")
-     start.addEventListener("click", function(){
-   start.hidden=true
-   howToPlay.hidden=true
-     })
+    //  howToPlay.addEventListener("click",function(){    document.open("instructions.html","How To Play", "width=600,height=600")})
+    //  const start=document.getElementById("start")
+    //  start.addEventListener("click", function(){
+  //  start.hidden=true
+  //  howToPlay.hidden=true
+    //  })
      
+    
      //Collison section
      
 
