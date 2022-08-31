3c02533bd0961b73cae1b5fd7f8d45d903afc7f0
Noel Fernandez <noelfernandez98@gmail.com>
2022-08-31 14:39:21 -0400

    random item loop updated

--- index.js
+++ index.js
@@ -19,17 +19,13 @@ loadSprite("onions", "onions.png")
 loadSprite("tomatoes", "tomatoes.png")
 loadSprite("bread1", "bread.png")
 loadSprite("bread2", "bread.png")
-loadSprite("cheese", "cheese.png")
+// loadSprite("cheese", "cheese.png")
 // Load assets
 loadSprite("bean", "plate.png")
 // Define player movement speed (pixels per second)
 const SPEED = 320
 // Add player game object
-const player = add([
-	sprite("bean"),
-	// center() returns the center point vec2(width() / 2, height() / 2)
-	pos(center()),
-])
+
 // onKeyDown() registers an event that runs every frame as long as user is holding a certain key
 onKeyDown("left", () => {
 	// .move() is provided by pos() component, move by pixels per second
@@ -38,96 +34,33 @@ onKeyDown("left", () => {
 onKeyDown("right", () => {
 	player.move(SPEED, 0)
 })
-onKeyDown("up", () => {
-	player.move(0, -SPEED)
-})
-onKeyDown("down", () => {
-	player.move(0, SPEED)
-})
-// onClick() registers an event that runs once when left mouse is clicked
+
+const options = ["meat", "lettuce", "onions", "bread2", "tomatoes"]
+const randomizer = () => options[Math.floor(Math.random() * arr.length)]
+
+const looper = () => {
+  loop(1, () => {
+    // add tree
+    add([
+      sprite(randomizer()),
+      pos(rand(vec2(width())).x, 10),
+      `tag1`,
+      area(),
+      scale(.2),
+      move(DOWN, 240),
+    ])
+  });
+}
+
+looper()
+
 
 add([
 	// text() component is similar to sprite() but renders text
 	text("Press arrow keys", { width: width() / 2 }),
 	pos(12, 12),
 ])
-loop(1, () => {
-    // add tree
-    add([
-      sprite('meat'),
-      pos(rand(vec2(width())).x, 10),
-      `tag1`,
-      area(),
-      scale(.2),
-      move(DOWN, 240),
-    ])
-  });
-  loop(1, () => {
-    // add tree
-    add([
-      sprite('lettuce'),
-      pos(rand(vec2(width())).x, 10),
-      `tag1`,
-      area(),
-      scale(.2),
-      move(DOWN, 240),
-    ])
-  });
-  loop(1, () => {
-    // add tree
-    add([
-      sprite('onions'),
-      pos(rand(vec2(width())).x, 10),
-      `tag1`,
-      area(),
-      scale(.2),
-      move(DOWN, 240),
-    ])
-  });
-  loop(1, () => {
-    // add tree
-    add([
-      sprite('bread1'),
-      pos(rand(vec2(width())).x, 10),
-      `tag1`,
-      area(),
-      scale(.2),
-      move(DOWN, 240),
-    ])
-  });
-  loop(1, () => {
-    // add tree
-    add([
-      sprite('bread2'),
-      pos(rand(vec2(width())).x, 10),
-      `tag1`,
-      area(),
-      scale(.2),
-      move(DOWN, 240),
-    ])
-  });
-  loop(1, () => {
-    // add tree
-    add([
-      sprite('tomatoes'),
-      pos(rand(vec2(width())).x, 10),
-      `tag1`,
-      area(),
-      scale(.2),
-      move(DOWN, 240),
-    ])
-  });
-  loop(1, () => {
-    // add tree
-    add([
-      sprite('tomatoes'),
-      pos(rand(vec2(width())).x, 10),
-      `tag1`,
-      area(),
-      scale(.2),
-      move(DOWN, 240),
-    ])
-  });
+
      const howToPlay=document.getElementById("instructions")
      howToPlay.addEventListener("click",function(){    document.open("instructions.html","How To Play", "width=600,height=600")})
      const start=document.getElementById("start")
