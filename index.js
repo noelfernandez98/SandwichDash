d238ac8fb6e2c20120876c5edd876c0ce54017c7
marcwayn8 <juniormarch48@gmail.com>
2022-09-01 11:45:15 +0000

    added intro,game logic, formmatted code and outro scene

--- index.js
+++ index.js
@@ -3,234 +3,325 @@ kaboom({
 	scale: 2,
 	// Set the default font
 	font: "sinko",
-})
-;
- loadSprite("clearsky", "DinerPic.jpg")
- loadSprite("meat", "meat.png")
+});
+
+//Timer
+var timeOfGame = 120
+// while(timeOfGame>0)
+// {
+// timeOfGame=timeOfGame-1}
+const li = () => { timeOfGame-- }
+let autocount = setInterval(li, 1000)
+var timerText = `Timer:${new Date((timeOfGame)* 1000).toISOString().substring(14, 19)}`;
+
+add([
+	text(`${timerText}`)
+])
+if (timeOfGame = 0) {
+	go("outro")
+}
+
+
+var score = 0
+loadSprite("clearsky", "DinerPic.jpg")
+loadSprite("meat", "meat.png")
 loadSprite("lettuce", "lettuce.png")
 loadSprite("onions", "onions.png")
 loadSprite("tomatoes", "tomatoes.png")
-loadSprite("bread1", "bread.png")
+loadSprite("bread", "bread.png")
 loadSprite("bread2", "bread.png")
 loadSprite("cheese", "cheese.png")
 // Load assets
 loadSprite("bean", "bread.png")
+loadSprite("bone", "bone1.png")
+loadSprite("bomb", "bomb1.png")
+loadSprite("boot", "boot1.png")
 
 
-scene('intro', ()=>{
+//Opening scene
+
+scene('intro', () => {
 	let background = add([
-	  sprite("clearsky"),
-	  pos(width() / 2, height() / 2),
-	  origin("center"),
-	  scale(1),
-	  fixed()
-	]);
-	
-	add([
-		
-		text("Welcome to SandWich Dash"),
-		pos(500,250)])
-		
-		
-		function addButton(txt, p, f) {
-
-	const btn = add([
-		text(txt),
-		pos(p),
-		area({ cursor: "pointer", }),
-		scale(1),
+		sprite("clearsky"),
+		pos(width() / 2, height() / 2),
 		origin("center"),
+		scale(1),
+		fixed()
+	]);
+
+	add([
+
+		text("Welcome to SandWich Dash"),
+		pos(500, 250)
 	])
 
-	btn.onClick(f)
+	//create Landing page buttons
+	function addButton(txt, p, f) {
 
-	btn.onUpdate(() => {
-		if (btn.isHovering()) {
-			const t = time() * 10
-			btn.color = rgb(
-				wave(0, 255, t),
-				wave(0, 255, t + 2),
-				wave(0, 255, t + 4),
-			)
-			btn.scale = vec2(1.2)
-		} else {
-			btn.scale = vec2(1)
-			btn.color = rgb()
-		}
+		const btn = add([
+			text(txt),
+			pos(p),
+			area({ cursor: "pointer", }),
+			scale(1),
+			origin("center"),
+		])
+
+		btn.onClick(f)
+
+		btn.onUpdate(() => {
+			if (btn.isHovering()) {
+				const t = time() * 10
+				btn.color = rgb(
+					wave(0, 255, t),
+					wave(0, 255, t + 2),
+					wave(0, 255, t + 4),
+				)
+				btn.scale = vec2(1.2)
+			}
+			else {
+				btn.scale = vec2(1)
+				btn.color = rgb()
+			}
+		})
+
+	}
+	addButton("start", (500, 100), function() {
+
+		go('game')
 	})
 
-}
-addButton("start",(200,350))
-addButton("How to Play",(550,350))
-	const start=document.getElementById("start")
-     start.addEventListener("click", function(){
-   start.hidden=true,
- 
-	go('game')
-     })
+	addButton("How to Play", (200, 200), function() { document.open("instructions.html", "How To Play", "width=600,height=600") })
 })
 go('intro')
 
-scene('game', ()=>{
-let background = add([
-	  sprite("clearsky"),
-	  pos(width() / 2, height() / 2),
-	  origin("center"),
-	  scale(1),
-	  fixed()
+
+
+//Ending scene
+scene('outro', () => {
+	let background = add([
+		sprite("clearsky"),
+		pos(width() / 2, height() / 2),
+		origin("center"),
+		scale(1),
+		fixed()
 	]);
-// Define player movement speed (pixels per second)
-const SPEED = 320;
-// Add player game object
-const player = add([
-	sprite("bean"),
-	scale(0.2),
-	// center() returns the center point vec2(width() / 2, height() / 2)
-	pos(center()),
-])
-// onKeyDown() registers an event that runs every frame as long as user is holding a certain key
-onKeyDown("left", () => {
-	// .move() is provided by pos() component, move by pixels per second
-	player.move(-SPEED, 0)
-})
-onKeyDown("right", () => {
-	player.move(SPEED, 0)
-})
-onKeyDown("up", () => {
-	player.move(0, -SPEED)
-})
-onKeyDown("down", () => {
-	player.move(0, SPEED)
-})
-// onClick() registers an event that runs once when left mouse is clicked
-onClick(() => {
-	// .moveTo() is provided by pos() component, changes the position
-	player.moveTo(mousePos())
-})
-var score=0
 
-add([
-	// text() component is similar to sprite() but renders text
-	text(`Score ${score}`),
-	pos(550, 12),
-])
+	add([
 
+		text("Sorry You Ran out of time"),
+		pos(500, 250)
+	])
 
+	//create Landing page buttons
+	function addButton(txt, p, f) {
 
-//Restart Button
-// const button=document.getElementById("button")
-// button.innerText="Restart"
-// button.addEventListener("click",function (){
-// 	  start.hidden=true,
- 
-// 	go('game')
-// })
-// add([
+		const btn = add([
+			text(txt),
+			pos(p),
+			area({ cursor: "pointer", }),
+			scale(1),
+			origin("center"),
+		])
 
-// 	text(`Restart ${button}`),
-// 		pos(30, 30),
-// 	])
-const options = ["meat", "lettuce", "onions", "bread2", "tomatoes"]
-const randomizer = () => options[Math.floor(Math.random() * options.length)]
+		btn.onClick(f)
 
-const looper = () => {
-  loop(1, () => {
-    add([
-      sprite(randomizer()),
-      pos(rand(vec2(width())).x, 10),
-      `tag1`,
-      area(),
-      scale(.2),
-      move(DOWN, 240),
-    ])
-  });
-}
+		btn.onUpdate(() => {
+			if (btn.isHovering()) {
+				const t = time() * 10
+				btn.color = rgb(
+					wave(0, 255, t),
+					wave(0, 255, t + 2),
+					wave(0, 255, t + 4),
+				)
+				btn.scale = vec2(1.2)
+			}
+			else {
+				btn.scale = vec2(1)
+				btn.color = rgb()
+			}
+		})
 
-looper()
+	}
+	addButton("Play Again", (500, 100), function() {
 
- add([
-  sprite("bread2"),
-  // pos(height() / 3, width() / 3),
-   pos(player / 5, width() / 3),
+		go('game')
+	})
 
-   scale(.1),
-  fixed()
-])
-
-add([
-	text("Press arrow keys", { width: width() / 2 }),
-	pos(12, 12),
-])
+	addButton("How to Play", (200, 200), function() { document.open("instructions.html", "How To Play", "width=600,height=600") })
 
 
-
-     const howToPlay=document.getElementById("instructions")
-     howToPlay.addEventListener("click",function(){    document.open("instructions.html","How To Play", "width=600,height=600")})
-     
-
-
-
+	add([
+		// text() component is similar to sprite() but renders text
+		text(`Your New High Score is ${score}`),
+		pos(550, 12),
+	])
 })
 
 
 
-loadSound("OtherworldlyFoe", "music.m4a")
+//Game logic scene
+
+scene('game', () => {
+	let background = add([
+		sprite("clearsky"),
+		pos(width() / 2, height() / 2),
+		origin("center"),
+		scale(1),
+		fixed()
+	]);
+
+
+	function addButton(txt, p, f) {
+
+		const btn = add([
+			text(txt),
+			pos(p),
+			area({ cursor: "pointer", }),
+			scale(1),
+			origin("center"),
+		])
+
+		btn.onClick(f)
+
+		btn.onUpdate(() => {
+			if (btn.isHovering()) {
+				const t = time() * 10
+				btn.color = rgb(
+					wave(0, 255, t),
+					wave(0, 255, t + 2),
+					wave(0, 255, t + 4),
+				)
+				btn.scale = vec2(1.2)
+			}
+			else {
+				btn.scale = vec2(1)
+				btn.color = rgb()
+			}
+		})
+
+	}
+
+	addButton("Restart", (90, 35), function() {
+
+		go('outro')
+	})
+
+
+	//Movement
+	// Define player movement speed (pixels per second)
+	const SPEED = 320;
+	// Add player game object
+	const player = add([
+		sprite("bean"),
+		scale(0.2),
+		area(),
+		// solid() component makes the object can't move pass other solid objects
+		solid(),
+		// center() returns the center point vec2(width() / 2, height() / 2)
+		pos(center()),
+	])
+	// onKeyDown() registers an event that runs every frame as long as user is holding a certain key
+	onKeyDown("left", () => {
+		// .move() is provided by pos() component, move by pixels per second
+		player.move(-SPEED, 0)
+	})
+	onKeyDown("right", () => {
+		player.move(SPEED, 0)
+	})
+	onKeyDown("up", () => {
+		player.move(0, -SPEED)
+	})
+	onKeyDown("down", () => {
+		player.move(0, SPEED)
+	})
+	// onClick() registers an event that runs once when left mouse is clicked
+	onClick(() => {
+		// .moveTo() is provided by pos() component, changes the position
+		player.moveTo(mousePos())
+	})
+
+
+	add([
+		// text() component is similar to sprite() but renders text
+		text(`Score ${score}`),
+		pos(550, 12),
+	])
+
+
+
+	//Randomizer
+	const toxic = ["boot", "bone", "bomb"]
+	const options = ["meat", "lettuce", "onions", "bread", "tomatoes", "cheese"]
+	const randomizer = () => options[Math.floor(Math.random() * options.length)]
+
+	const looper = () => {
+		loop(5, () => {
+			add([
+				sprite(randomizer()),
+				pos(rand(vec2(width())).x, 10),
+				`tag1`,
+				area(),
+				solid(),
+				scale(.2),
+				move(DOWN, 240),
+				'friendly'
+			])
+		});
+	}
+
+	looper()
+	const randomizer2 = () => toxic[Math.floor(Math.random() * toxic.length)]
+
+	const looper2 = () => {
+		loop(1, () => {
+			add([
+				sprite(randomizer2()),
+				pos(rand(vec2(width())).x, 10),
+				`tag1`,
+				area(),
+				solid(),
+				scale(.2),
+				move(DOWN, 240),
+				"enemy"
+			])
+		});
+	}
+
+	looper2()
+
+
+	add([
+		text("Press arrow keys", { width: width() / 2 }),
+		pos(12, 12),
+	])
+
+
+//Collision
+	player.onCollide("friendly", (food) => {
+		destroy(food),
+			score += 10
+		wait(10, () => {
+			destroy(food)
+		})
+	})
+	player.onCollide("enemy", (enemy) => {
+		destroy(enemy),
+			score -= 10
+		wait(3, () => {
+			destroy(enemy)
+		})
+	})
+})
+
+
+//Music
+loadSound("music", "music.m4a")
 
 // play() to play audio
 // (This might not play until user input due to browser policy)
-const music = play("OtherworldlyFoe", {
+const music = play("music", {
 	loop: true,
 })
 
 // Adjust global volume
 volume(0.5)
-
-const label = add([
-	text(),
-])
-
-function updateText() {
-	label.text = `
-${music.isPaused() ? "Paused" : "Playing"}
-Time: ${music.time().toFixed(2)}
-Tolume: ${music.volume().toFixed(2)}
-Tetune: ${music.detune().toFixed(2)}
-	`.trim()
-}
-
-updateText()
-
-// Update text every frame
-onUpdate(updateText)
-
-// Adjust music properties through input
-onKeyPress("space", () => {
-	if (music.isPaused()) {
-		music.play()
-	} else {
-		music.pause()
-	}
-})
-
-onKeyPress("up", () => music.volume(music.volume() + 0.1))
-onKeyPress("down", () => music.volume(music.volume() - 0.1))
-onKeyPress("left", () => music.detune(music.detune() - 100))
-onKeyPress("right", () => music.detune(music.detune() + 100))
-onKeyPress("escape", () => music.stop())
-
-const keyboard = "awsedftgyhujk"
-
-// Simple piano with "bell" sound and the second row of a QWERTY keyboard
-for (let i = 0; i < keyboard.length; i++) {
-	onKeyPress(keyboard[i], () => {
-		play("bell", {
-			// The original "bell" sound is F, -500 will make it C for the first key
-			detune: i * 100 - 500,
-		})
-	})
-}
-
-
-onCollide("bean","lettuce",() => {
-  	score++
-})
